8b8f205ffffed104361ea59151c96e4ef8d6f846
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-07-05 22:33:19 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,12 +1,10 @@
-class Puppy
+class Puppy 
+  attr_accessor :name, :breed, :months_old 
 
-  attr_accessor :months_old, :name
-  attr_reader :breed
+   def initialize(name, breed, months_old)
+    @name = name 
+    @breed = breed 
+    @months_old = months_old 
+  end 
 
-  def initialize(name, breed, months_old)
-    @name = name
-    @breed = breed
-    @months_old = months_old
-  end
-
-end
+ end
